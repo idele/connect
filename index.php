7ba828ec09e236134cc7229b30a332fa51f3ba71
idele <1332336543@qq.com>
2016-04-20 09:46:38 +0800

    test
    test1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	 <!--  <link rel="stylesheet" type="text/css" href="boot/button.css">-->
+  	 <!--  <link rel="stylesheet" type="text/css" href="../plugins/uploadify/uploadify.css"> -->
+    <!-- Bootstrap -->
+    <link href="boot/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="boot/js/jquery.js"></script>
+    <script src="boot/js/bootstrap.min.js"></script>
+  <!--  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
+  </body>
+</html>
